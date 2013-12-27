e0feecc0e09ccaa2b5035cf6540ca43bdc775ad2
DaveDson <throwawaydd@gmail.com>
2013-12-27 00:49:37 +0000

    Adding combat code and a test NPC

--- main.py
+++ main.py
@@ -46,17 +46,17 @@ MENU_HILIGHT = libtcod.Color(120, 153, 34)
 #Setting custom font. Later this might be changed to allow custom tilesets.
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-#Initialising the root console.
+#Initialising the root console
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Ponyhack', False)
 
-#The virtual console the map will be drawn on before blitting to root.
+#The virtual console the map will be drawn on before blitting to root
 mapcon = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
 
-#The panel with various game information.
+#The panel with various game information
 panel = libtcod.console_new(PANEL_WIDTH, PANEL_HEIGHT)
 
 class Area:
-	#Class for area objects.
+	#Class for area objects
 	def __init__(self, X, Y, map, name, objects, dark=False):
 		global areas		
 		self.X = X
@@ -65,13 +65,13 @@ class Area:
 		self.map = map
 		self.objects = objects
 		self.dark = dark
-		#Append a new area to the areas dictionary.
+		#Append the new area to the areas dictionary
 		if not self.X in areas:
 			areas[X] = {}
 		areas[X][Y] = self
 
 class Object:
-	#Generic class for all map objects.
+	#Generic class for all map objects
 	def __init__(self, x, y, X, Y, name, char, color, blocks=False, inventory=[], light_emittance=0, creature=None, item=None):
 		self.x = x
 		self.y = y
@@ -84,7 +84,7 @@ class Object:
 		self.inventory = inventory
 		self.light_emittance = light_emittance
 		self.light_map = None
-		#tell any components what owns them
+		#Tell any components what owns them
 		self.creature = creature
 		if self.creature:
 			self.creature.owner = self
@@ -95,7 +95,7 @@ class Object:
 	def move(self, dx, dy):
 		x = self.x + dx
 		y = self.y + dy
-		#change the map area if going off screen
+		#Change the map area if going off screen
 		if x < 0:
 			if self.change_area(-1, 0):
 				self.x = MAP_WIDTH - 1
@@ -108,7 +108,7 @@ class Object:
 		elif y > MAP_HEIGHT - 1:
 			if self.change_area(0, 1):
 				self.y = 0
-		#Otherwise check whether the tile is blocked and if not, move to it.
+    #Otherwise check whether the tile is blocked and if not, move to it.
 		elif not current_area.map[x][y].blocked:
 			self.x = x
 			self.y = y
@@ -116,31 +116,32 @@ class Object:
 	def change_area(self, dX, dY):
 		#Changing the area that an object exists in.
 		global current_area
-		new_X = self.X + dX
-		new_Y = self.Y + dY
-		if new_X in areas:
-			if new_Y in areas[new_X]:
-				areas[new_X][new_Y].objects.append(self)
+		X = self.X + dX
+		Y = self.Y + dY
+		if X in areas:
+			if Y in areas[X]:
+				areas[X][Y].objects.append(self)
 				areas[self.X][self.Y].objects.remove(self)
-				self.X = new_X
-				self.Y = new_Y
-				current_area = areas[new_X][new_Y]
+				self.X = X
+				self.Y = Y
+				current_area = areas[X][Y]
 				initialise_fov()
 				return True
 		return False
 
 	def draw(self):
 		#Drawing the object to the map.
-		libtcod.console_set_default_foreground(mapcon, self.color)
-		libtcod.console_put_char(mapcon, self.x, self.y, self.char, libtcod.BKGND_NONE)
+		if is_visible(self.x, self.y):
+			libtcod.console_set_default_foreground(mapcon, self.color)
+			libtcod.console_put_char(mapcon, self.x, self.y, self.char, libtcod.BKGND_NONE)
 
 	def clear(self):
-		#clearing the object from the map.
+		#Clearing the object from the map.
 		libtcod.console_put_char(mapcon, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
 class Creature:
 	#Component for all creature objects.
-	def __init__(self, hp, stamina, strength, dexterity, toughness):
+	def __init__(self, hp, stamina, strength, dexterity, toughness, death_function=None):
 		self.max_hp = hp
 		self.hp = hp
 		self.max_stamina = stamina
@@ -148,16 +149,22 @@ class Creature:
 		self.strength = strength
 		self.dexterity = dexterity
 		self.toughness = toughness
+		self.death_function = death_function
 
 	def attack(self, target):
-		damage = self.strength - target.toughness
+		damage = self.strength - target.creature.toughness
 
 		if damage > 0:
-			target.take_damage(damage)
+			message('The ' + self.owner.name + ' attacks the ' + target.name + ' for ' + str(damage) + ' damage.')
+			target.creature.take_damage(damage)
+		else:
+			message('The ' + self.owner.name + ' attacks the ' + target.name + ' but the blow glances away!')
 
 	def take_damage(self, damage):
 		if damage > 0:
 			self.hp -= damage
+			if self.hp <= 0 and self.death_function != None:
+				self.death_function(self.owner)
 
 	def heal(self, amount):
 		self.hp += amount
@@ -199,21 +206,21 @@ def handle_keys():
 	if game_state == 'playing':
 		#movement keys
 		if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
-			player_move(0, -1)
+			player_move_or_attack(0, -1)
 		elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
-			player_move(0, 1)
+			player_move_or_attack(0, 1)
 		elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
-			player_move(-1, 0)
+			player_move_or_attack(-1, 0)
 		elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
-			player_move(1, 0)
+			player_move_or_attack(1, 0)
 		elif key.vk == libtcod.KEY_KP7:
-			player_move(-1, -1)
+			player_move_or_attack(-1, -1)
 		elif key.vk == libtcod.KEY_KP9:
-			player_move(1, -1)
+			player_move_or_attack(1, -1)
 		elif key.vk == libtcod.KEY_KP1:
-			player_move(-1, 1)
+			player_move_or_attack(-1, 1)
 		elif key.vk == libtcod.KEY_KP3:
-			player_move(1, 1)
+			player_move_or_attack(1, 1)
 		elif key.vk == libtcod.KEY_KP5:
 			pass #wait a turn
 		else:
@@ -250,32 +257,47 @@ def handle_keys():
 
 			return 'didnt-take-turn'
 
-def player_move(x, y):
+def player_move_or_attack(dx, dy):
 	global fov_recompute
-	player.move(x, y)
-	fov_recompute = True
+	
+	x = player.x + dx
+	y = player.y + dy
+
+	#try to find a target to attack
+	target = None
+	for object in current_area.objects:
+		if object.creature and object.x == x and object.y == y:
+			target = object
+			break
+
+	if target is not None:
+		player.creature.attack(target)
+
+	else:
+		player.move(dx, dy)
+		fov_recompute = True
 
 def menu(header, options, width,):
-	#the player is presented with some options and makes a choice based on graphics
+	#The player is presented with some options and makes a choice based on graphics
 	choice = 0
 	new_choice = 0
 
-	#calculate total height for header (after auto-wrap) and one line per option
+	#Calculate total height for header (after auto-wrap) and one line per option
 	header_height = libtcod.console_get_height_rect(mapcon, 0, 0, width, SCREEN_HEIGHT, header)
 	height = len(options) + header_height
 
-	#create the virtual console to write the menu on
+	#Create the virtual console to write the menu on
 	window = libtcod.console_new(width, height)
 
 	while True:
-		#clear the console ready to draw
+		#Clear the console ready to draw
 		libtcod.console_clear(window)
 
-		#draw the header
+		#Craw the header
 		libtcod.console_set_default_foreground(window, libtcod.white)
 		libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
 
-		#iterate through and print the options, highlighting the current selection.
+		#Iterate through and print the options, highlighting the current selection.
 		y = header_height
 		for index, option in enumerate(options):
 			if index == choice:
@@ -288,12 +310,12 @@ def menu(header, options, width,):
 				libtcod.console_print_ex(window, 1, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
 			y += 1
 
-		#blit the window to the root and flush to render everything.
+		#Blit the window to the root and flush to render everything.
 		libtcod.console_blit(window, 0, 0, width, height, 0, SCREEN_WIDTH/2 - width/2, SCREEN_HEIGHT/2 - height/2)
 		libtcod.console_flush()
 
 		
-		#up and down arrows change selection
+		#Up and down arrows change selection
 		libtcod.sys_wait_for_event(libtcod.EVENT_KEY_PRESS, key, mouse, True)	
 		if key.vk == libtcod.KEY_ENTER:
 			return choice
@@ -301,21 +323,30 @@ def menu(header, options, width,):
 			new_choice = choice - 1
 		elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
 			new_choice = choice + 1
-		#check that we're not selecting outside the boundary
+		#Check that we're not selecting outside the boundary
 		if 0 <= new_choice < len(options):
 			choice = new_choice
+
+def monster_death(monster):
+	#Death function for a generic monster.
+	monster.char = '%'
+	monster.color = libtcod.dark_red
+	monster.blocks = False
+	monster.creature = None
+	message('The ' + monster.name +' has died!')
+	monster.name = 'remains of ' + monster.name
 
 def initialise_fov():
 	global fov_recompute, fov_map, light_map
 	fov_recompute = True
 
-	#create the FOV map
+	#Create the FOV map
 	fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
 	for y in range(MAP_HEIGHT):
 		for x in range(MAP_WIDTH):
 			libtcod.map_set_properties(fov_map, x, y, not current_area.map[x][y].block_sight, not current_area.map[x][y].blocked)
 
-	#initialise the light maps for the objects
+	#Initialise the light maps for the objects
 	for object in current_area.objects:
 		if object.light_emittance != 0:
 			object.light_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
@@ -327,7 +358,7 @@ def render_all():
 	global fov_map, fov_recompute
 	map = current_area.map
 
-	#define the upper left corner of the visible map
+	#Define the upper left corner of the visable map
 	start_x = player.x - MAP_WINDOW_WIDTH/2
 	if start_x < 0:
 		start_x = 0
@@ -339,61 +370,61 @@ def render_all():
 	elif start_y > MAP_HEIGHT - MAP_WINDOW_HEIGHT:
 		start_y = MAP_HEIGHT - MAP_WINDOW_HEIGHT
 	if fov_recompute:
-		#compute the FOV
+		#Compute the FOV
 		fov_recompute = False
 		libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, libtcod.FOV_PERMISSIVE(1))
 
-		#recompute the light maps for each object
+		#Recompute the light maps for each object
 		for object in current_area.objects:
 			if object.light_emittance > 0:
 				libtcod.map_compute_fov(object.light_map, object.x, object.y, object.light_emittance, FOV_LIGHT_WALLS, FOV_ALGO)
 
-		#clear the map console ready to draw
+		#Clear the map console ready to draw
 		libtcod.console_clear(mapcon)
 
-		#render the map console
+		#Render the map console
 		for y in range(start_y, MAP_HEIGHT):
 			for x in range(start_x, MAP_WIDTH):
-				#check that each tile is visable and lit
-				visible = libtcod.map_is_in_fov(fov_map, x, y)
-				lit = is_lit(x, y)
-				if visible and lit:
+				#Check that each tile is visable and lit
+				visible = is_visible(x, y)
+				if visible:
 					libtcod.console_set_char_background(mapcon, x, y, map[x][y].color(True), libtcod.BKGND_SET)
 					map[x][y].explored = True
-				#if not, check whether it is explored.
+				#If not, check whether it is explored.
 				elif map[x][y].explored:
 					libtcod.console_set_char_background(mapcon, x, y, map[x][y].color(False))
 
-	#draw all the objects in the current area
+	#Draw all the objects in the current area
 	for object in current_area.objects:
 		object.draw()
-	#blit the map to the screen
+	#Blit the map to the screen
 	libtcod.console_blit(mapcon, start_x, start_y, MAP_WINDOW_WIDTH, MAP_WINDOW_HEIGHT, 0, 0, 0)
 	render_panel()
 
-def is_lit(x, y):
-	#determines whether a map tile is lit or not.
-	for object in current_area.objects:
-		if object.light_map:
-			if libtcod.map_is_in_fov(object.light_map, x, y):
-				return True
+def is_visible(x, y):
+	#Determines whether a map tile is lit or not.
+	if libtcod.map_is_in_fov(fov_map, x, y):
+		for object in current_area.objects:
+			if object.light_map:
+				if libtcod.map_is_in_fov(object.light_map, x, y):
+					return True
 	return False
 
 def render_panel():
 	global game_msgs
-	#render the information panel on the right side of the screen
+	#Render the information panel on the right side of the screen
 
-	#clear the panel ready to render
+	#Clear the panel ready to render
 	libtcod.console_set_default_background(panel, libtcod.black)
 	libtcod.console_clear(panel)
 
-	#render the HP bar
+	#Render the HP bar
 	render_bar(1, 1, PANEL_WIDTH - 2, 'HP', player.creature.hp, player.creature.max_hp, libtcod.red, libtcod.dark_red)
 
-	#render the stamina bar
+	#Render the stamina bar
 	render_bar(1, 3, PANEL_WIDTH - 2, 'STA', player.creature.stamina, player.creature.max_stamina, libtcod.blue, libtcod.dark_blue)
 
-	#write the game messages
+	#Write the game messages
 	y = 0
 	for line, color in game_msgs:
 		libtcod.console_set_default_foreground(panel, color)
@@ -403,14 +434,14 @@ def render_panel():
 	libtcod.console_blit(panel, 0, 0, PANEL_WIDTH, PANEL_HEIGHT, 0, MAP_WINDOW_WIDTH, 0)
 
 def render_bar(x,y,total_width, name, value, maximum, bar_color, back_color):
-	#render a bar (HP, EXP etc.). First, calculate the width of the bar.
+	#Render a bar (HP, EXP etc.). First, calculate the width of the bar.
 	bar_width = int(float(value) / maximum * total_width)
 
-	#render the background first
+	#Render the background first
 	libtcod.console_set_default_background(panel, back_color)
 	libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
 
-	#now render the bar on top
+	#Now render the bar on top
 	libtcod.console_set_default_background(panel, bar_color)
 	if bar_width > 0:
 		libtcod.console_rect(panel, x, y, bar_width, 1, False, libtcod.BKGND_SCREEN)
@@ -421,11 +452,11 @@ def render_bar(x,y,total_width, name, value, maximum, bar_color, back_color):
 
 def message(new_msg, color=libtcod.white):
 	global game_msgs, panel
-	#split the message amongst multiple lines
+	#Split the message amongst multiple lines
 	new_msg_lines = textwrap.wrap(new_msg, MSG_WIDTH)
 
 	for line in new_msg_lines:
-		#if the message log is too long, delete the earliest line
+		#If the message log is too long, delete the earliest line
 		if len(game_msgs) == MSG_HEIGHT:
 			del game_msgs[0]
 
@@ -438,13 +469,15 @@ def new_game():
 	north = Area(0, -1, mapgen.ponyville_north(), 'ponyville north', [])
 	current_area = Area(0, 0, mapgen.ponyville(), 'Central Ponyville', [])
 	#creature_component = Creature(hp=20, stamina=20, strength=5, dexterity=5, toughness=5)
-	player = Object(MAP_WIDTH/2, MAP_HEIGHT/2, 0, 0, 'Player', '@', libtcod.white, light_emittance=12, creature=Creature(20, stamina=20, strength=5, dexterity=5, toughness=5))
+	player = Object(MAP_WIDTH/2, MAP_HEIGHT/2, 0, 0, 'Player', '@', libtcod.white, light_emittance=12, creature=Creature(20, stamina=20, strength=5, dexterity=5, toughness=3))
+	practice_dummy = Object(MAP_WIDTH/2, MAP_HEIGHT/2 + 2, 0, 0, 'Practice Dummy', 'd', libtcod.red, creature=Creature(hp=10, stamina=10, strength=3, dexterity=5, toughness=2, death_function=monster_death))
 	inv = player.inventory
 	sword = Object(MAP_WIDTH/2 + 2, MAP_HEIGHT/2, 0, 0, 'Sword', '/', libtcod.darker_red, light_emittance=8, item=Item())
 	shield = Object(MAP_WIDTH/2 - 2, MAP_HEIGHT/2, 0, 0, 'Shield', '[', libtcod.darker_blue, item=Item())
 	current_area.objects.append(sword)
 	current_area.objects.append(shield)
 	current_area.objects.append(player)
+	current_area.objects.append(practice_dummy)
 	initialise_fov()
 
 	game_state = 'playing'
@@ -465,7 +498,7 @@ def play_game():
 		libtcod.console_flush()
 		player_action = handle_keys()
 		if player_action == 'exit':
-			break
+			break #Exit the game
 
 def main_menu():
 	while not libtcod.console_is_window_closed():
@@ -476,7 +509,7 @@ def main_menu():
 		if choice == 0:
 			play_game()
 		elif choice == 1:
-			break #Exit the game.
+			break
 
 new_game()
 main_menu()
